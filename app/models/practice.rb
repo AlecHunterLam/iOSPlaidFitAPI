da93187f3935117ba27f4c566b0b4a655ed788c0
Winston Chu <wpchu@andrew.cmu.edu>
2018-03-04 21:28:00 -0500

    practice and team calculation models done

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -1,2 +1,18 @@
 class Practice < ApplicationRecord
+
+    # Relationships
+    belongs_to :team
+
+    # Scopes
+
+    # Validations
+    validates_presence_of :team_id, :duration, :difficulty, :date
+    duration_array = [0] + (0..120).to_a
+    difficulty_array = [0] + (1..10).to_a
+    validates :duration, numericality: { only_integer: true }, inclusion: { in: duration_array }
+    validates :difficulty, numericality: { only_integer: true }, inclusion: { in: difficulty_array }
+    validates_date :date
+
+    # Methods
+
 end
--- app/models/team_calculation.rb
+++ app/models/team_calculation.rb
@@ -1,2 +1,33 @@
 class TeamCalculation < ApplicationRecord
+
+    # Array constants
+    SEASONS = [['Fall', 'fall'], ['Winter', 'winter'], ['Spring', 'spring']]
+
+    # Relationships
+    belongs_to :team
+
+    # Scopes
+
+    # Validations
+    validates_presence_of :team_id, :week_of
+    validates_date :week_of
+    validates :sleep, numericality: true
+    validates :hydration, numericality: true
+    validates :stress, numericality: true
+    validates :load, numericality: true
+    validates_inclusion_of :season, in: SEASONS.map{|key, value| value}, message: "is not an option"
+    validate :week_of_on_sunday
+
+    # Methods
+
+    private
+
+    # Makes sure that the week_of variable 
+    def week_of_on_sunday
+        if self.week_of.strftime("%A") == "Sunday"
+            return true
+        end
+        false
+    end
+
 end
