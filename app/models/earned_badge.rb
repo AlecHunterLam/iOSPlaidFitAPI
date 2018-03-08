6ca0279d2aaec115a39ddadefd08be58b66f8ec4
Alec Lam <alechunterlam@gmail.com>
2018-03-08 16:43:35 -0500

    earned badges done

--- app/models/earned_badge.rb
+++ app/models/earned_badge.rb
@@ -1,2 +1,15 @@
 class EarnedBadge < ApplicationRecord
+  # Relationships
+  belongs_to :user
+  belongs_to :badges
+
+  # Scopes
+
+  # Validations
+  validates_presence_of :user_id, :badge_id, :date_earned
+  validates_date :date_earned, on_or_before: Date.current
+
+  # Methods
+
+
 end
