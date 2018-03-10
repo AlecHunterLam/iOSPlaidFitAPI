b024f56f60c66e9b643afa717e699add41e4b718
Alec Lam <alechunterlam@gmail.com>
2018-03-09 23:22:15 -0500

    wrote event controller, added scope to events (chronological)

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,54 @@
+class EventsController < ApplicationController
+  # Controller Code
+
+  before_action :set_event, only: [:show, :update, :destroy]
+
+  # get /events
+  def index
+    @events = Event.all
+
+    render json: @events
+  end
+
+  # get /events/1
+  def show
+    render json: @event
+  end
+
+  # post /event
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      render json: @event, status: :created, location: @event
+    else
+      render json: @event.errors, status: :unprocessable_entity
+    end
+  end
+
+  # patch/put /events/1
+  def update
+    if @event.update(event_params)
+      render json: @event
+    else
+      render json: @child.erros, status: :unprocessable_entity
+    end
+  end
+
+  # delete /event/1
+  def destroy
+    @event.destroy
+  end
+
+  private
+  # callbacks for common setup
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  # white list of parameters
+  def child_params
+    params.permit(:player_id, :description, :event_time)
+  end
+
+end
--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,7 @@ class Event < ApplicationRecord
   # Scopes
   scope :upcoming,      -> { where('? <= event_time', Time.now) }
   scope :past,          -> { where('event_time <= ?', Time.now) }
-
+  scope :chronological, -> { order(:event_time) }
   # Validations
   validates_presence_of :user_id, :description, :event_time
   validates_timeliness_of :event_time, on_or_after: Time.now
