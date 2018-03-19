c7be0343f569c52d12628a56de60e85e05f61d97
Alec Lam <alechunterlam@gmail.com>
2018-03-19 09:14:42 -0400

    completed swaggerdocs for notifications_controller.rb

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -1,5 +1,46 @@
 module Api::V1
   class NotificationsController < ApplicationController
+    # documentation
+    swagger_controller :notifications, "Notification Management"
+
+    swagger_api :index do
+      summary "Fetches all Notifications"
+      notes "This lists all of the notifications"
+    end
+
+    swagger_api :show do
+      summary "Shows one Notification"
+      params :path, :id, :integer, :required, "Notification ID"
+      notes "This lists details of one notification"
+      response :not_found
+    end
+
+    swagger_api :create do
+      summary "Creates a new Notification"
+      params :form, :sender_id, :integer, :required, "Sender ID"
+      params :form, :receiver_id, :integer, :required, "Receiver ID"
+      params :form, :message, :string, :required, "Message"
+      params :form, :priority, :string, "Priority"
+      response :not_acceptable
+    end
+
+    swagger_api :update do
+      summary "Updates an existing Notification"
+      params :path, :id, :integer, :required, "Notification ID"
+      params :form, :sender_id, :integer, :required, "Sender ID"
+      params :form, :receiver_id, :integer, :required, "Receiver ID"
+      params :form, :message, :string, :required, "Message"
+      params :form, :priority, :string, "Priority"
+      response :not_found
+      response :not_acceptable
+    end
+
+    swagger_api :destroy do
+      summary "Deletes an existing Notification"
+      params :path, :id, :integer, :required, "Notification ID"
+      response :not_found
+    end
+
     before_action :set_notification, only: [:show, :update, :destroy]
 
     # GET /notifications
@@ -16,6 +57,7 @@ module Api::V1
     # POST /notifications
     def create
       @notification = Notification.new(notification_params)
+      @notification.notified_time = Time.now
       if @notification.save
         render json: @notification, status: :created, location: [:v1, @notification]
       else
