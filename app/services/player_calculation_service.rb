38b70cd926a0ec9c2b8e72b279a9a7e434beee7f
Alec Lam <alechunterlam@gmail.com>
2018-04-01 14:50:19 -0400

    working on services

---
+++ app/services/player_calculation_service.rb
@@ -0,0 +1,60 @@
+# calculated at the end of each week (monday-monday) for each player
+# run the cron job sunday night
+class PlayerCalculationService
+  def initialize(user_object)
+    @user_id = user_object.id
+    @player_calculation = PlayerCalculation.new
+
+    # set the fields
+    @player_calculation.player_id = @user_id
+  end
+
+
+  private
+  def self.get_sleep_average
+  end
+
+  def self.get_hydration_average
+  end
+
+  def self.get_soreness_average
+  end
+
+  def self.get_load_average
+  end
+
+  def self.get__average
+  end
+
+  def self.get_sleep_average
+  end
+  create_table "player_calculations", force: :cascade do |t|
+    t.integer "player_id"
+    t.float "sleep_average"
+    t.float "hydration_average"
+    t.float "soreness_average"
+    t.float "load_average"
+    t.string "season"
+    t.integer "season_rank"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float "weekly_load"
+    t.float "weekly_strain"
+    t.float "life_stress_average"
+    t.float "academic_stress_average"
+    t.float "sleep_quality_average"
+    t.float "hydration_quality_average"
+    t.float "personal_performance_average"
+    t.float "practice_difficulty_average"
+    t.datetime "week_of"
+  end
+
+
+
+
+
+
+
+
+
+end
---
+++ app/services/survey_service.rb
@@ -0,0 +1,65 @@
+# calculated at the end of each week (monday-monday) for each player
+# run the cron job sunday night
+class SurveyService
+  def initialize(params)
+    @player_id = params[:player_id]
+    @type = params[:survey_type]
+    @response = params[:response]
+    @team_id = params[:team_id]
+    @completed = Time.now
+    @season = # # # # # # # # # # # #
+    @session_load = nil
+    @daily_load = nil
+    @monotony = nil
+    @dailt_strain = nil
+
+    @survey = Survey.new
+    # set the fields
+    @player_calculation.player_id = @user_id
+  end
+
+
+  def set_provided_survey_params
+    @survey.player_id = @player_id
+    @survey.type = @type
+    @survey.response = @response
+    @survey.completed = @completed
+  end
+
+  def perform_survey_calculations
+    # set the season for the survey
+    players_team = Team.find(@team_id)
+    season = players_team.season.capitalize
+    @survey.season = season + '-' + (Time.now.year).to_s
+    # calculate session load
+    current_day = (Time.now.day).to_s
+    current_year = (Time.now.year).to_s
+    current_month = (Time.now.month).to_s
+
+    Time.parse(current_year + '-' + current_month + '-' + current_day + ' 00:00:00') # => Fri, 31 Dec 1999 14:00:00 HST -10:00
+
+    practice_duration = Practice.
+
+  end
+
+  create_table "surveys", force: :cascade do |t|
+    t.integer "player_id" X
+    t.string "type" X
+    t.string "response" X
+    t.datetime "completed" X
+    t.string "season" X
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float "session_load"
+    t.float "daily_load"
+    t.float "daily_strain"
+    t.float "monotony"
+  end
+
+
+
+
+  private
+
+
+end
