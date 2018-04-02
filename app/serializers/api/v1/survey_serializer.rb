4e559aa105460be02b7ca94a66a1eb22624e1226
Alec Lam <alechunterlam@gmail.com>
2018-04-02 03:27:38 -0400

    added new relation to serializers

--- app/serializers/api/v1/survey_serializer.rb
+++ app/serializers/api/v1/survey_serializer.rb
@@ -2,5 +2,6 @@ module Api::V1
   class SurveySerializer < ActiveModel::Serializer
     attributes :id, :user_id, :type, :response, :completed, :season
     belongs_to :user
+    belongs_to :practie, optional: true
   end
 end
