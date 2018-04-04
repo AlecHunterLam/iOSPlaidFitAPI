a0e50880c3e96790477b354388c5ad0cee2f0701
Aathreya Thuppul <aathreya.thuppul@gmail.com>
2018-04-03 23:51:49 -0400

    created team context

---
+++ test/sets/team_contexts.rb
@@ -0,0 +1,11 @@
+module Teams
+  module TeamContexts
+    def create_teams
+      @team1 = @msoccer = Team.create(sport: “soccer”, gender: “Men”, season: "fall", active: true)
+    end
+
+    def delete_teams
+      @team1.delete
+    end
+  end
+end   
