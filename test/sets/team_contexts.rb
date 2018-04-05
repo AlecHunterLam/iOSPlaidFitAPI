ced22f25aa7ee39e09a07f17e172c80d0d7662a0
Aathreya Thuppul <aathreya.thuppul@gmail.com>
2018-04-05 02:27:00 -0400

    made a few users

--- test/sets/team_contexts.rb
+++ test/sets/team_contexts.rb
@@ -1,11 +1,11 @@
 module Contexts
   module TeamContexts
     def create_teams
-      @team1 = @msoccer = Team.create(sport: “soccer”, gender: “Men”, season: "fall", active: true)
+      @msoccer = Team.create(sport: “soccer”, gender: “Men”, season: "fall", active: true)
     end
 
     def delete_teams
-      @team1.delete
+      @msoccer.delete
     end
   end
 end
--- test/sets/user_contexts.rb
+++ test/sets/user_contexts.rb
@@ -1,4 +1,6 @@
 module Contexts
   module UserContexts
     def create_users
+      @alec = User.create(first_name: "Alec", last_name: "Lam", andrew_id: "ahlam", email: "ahlam@andrew.cmu.edu", major: "Information Systems", phone: "1111111111", role: "player", active: true, year: "Junior")
+      @greg = User.create(first_name: "Greg", last_name: "Bellwoar", andrew_id: "gbellwoar", email: "gbellwoar@andrew.cmu.edu", major: "Information Systems", phone: "1111111111", role: "player", active: true, year: "Junior")
       
