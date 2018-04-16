355ad307aeb6af9cc6efaa56bcbdc76dd0d5b080
Alec Lam <alechunterlam@gmail.com>
2018-04-15 21:42:33 -0400

    added fields to users fot token authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180406021333) do
+ActiveRecord::Schema.define(version: 20180416014129) do
 
   create_table "badges", force: :cascade do |t|
     t.string "badge_name"
@@ -166,6 +166,8 @@ ActiveRecord::Schema.define(version: 20180406021333) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "year"
+    t.string "password_digest"
+    t.string "api_key"
   end
 
 end
