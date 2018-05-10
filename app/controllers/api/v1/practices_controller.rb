7da8e962c65f9c467a158c42fd5ef12eee6be25e
Alec Lam <alechunterlam@gmail.com>
2018-05-10 00:36:36 -0400

    functioning multiple practice adding thing

--- app/controllers/api/v1/practices_controller.rb
+++ app/controllers/api/v1/practices_controller.rb
@@ -18,9 +18,11 @@ module Api::V1
         swagger_api :create do
             summary "Creates a new Practice"
             param :form, :team_id, :integer, :required, "Team ID"
-            param :form, :duration, :integer, :required, "Duration"
-            param :form, :difficulty, :integer, :required, "Difficulty"
+            param :form, :duration, :integer, :optional, "Duration"
+            param :form, :difficulty, :integer, :optional, "Difficulty"
             param :form, :practice_time, :datetime, :required, "Date"
+            param :form, :recurring_weekly, :boolean, :optional, "whether you want to repeat this over more than one weeks"
+            param :form, :num_weeks, :int, :optional, "weeks to repeat for"
             response :not_acceptable
         end
 
@@ -56,36 +58,46 @@ module Api::V1
 
         # POST /practices
         def create
-            weeks_repeated = practice_params[:num_weeks]
+            weeks_repeated = practice_params[:num_weeks].to_i
             recurring_weekly = practice_params[:recurring_weekly]
-
+            practice_duration = practice_params[:duration].to_i
+            practice_difficulty = practice_params[:difficulty].to_i
             # default the practice duration to be 2 hours, difficulty 5
-            if practice_params[:practice_time].nil?
-              practice_params[:practice_time] = 120
+
+            # to_i returns 0 for strings like ""
+            if practice_params[:duration] != "0" || practice_duration == 0
+              practice_duration = 120
             end
-            if practice_params[:difficulty].nil?
-              practice_params[:difficulty] = 5
+            # check raw, check conversion
+            if practice_params[:difficulty] == "0" && practice_difficulty == 0
+              practice_difficulty = 0
+            elsif practice_params[:difficulty] != "0" && practice_difficulty == 0
+              practice_difficulty = 5
             end
 
+            puts '1-' + (recurring_weekly).to_s
+            puts '2-' + (recurring_weekly && (weeks_repeated.is_a? Integer)).to_s
+            puts '3-' + ((weeks_repeated.is_a? Integer)).to_s
 
-            if (weeks_repeated && (weeks_repeated.is_a? Integer))
+            if (recurring_weekly && (weeks_repeated.is_a? Integer))
               if weeks_repeated < 1
                 render json: { errors: "invalid number of repeated weeks" }, status: :unprocessable_entity
                 return
               end
-              week_offset = 86400
 
               repeat_count = 0
               all_practices = []
               loop do
-                practice_datetime = practice_params[:practice_time]
+                practice_datetime = practice_params[:practice_time] + ' ' + Time.now.strftime( "%z" )
                 if practice_datetime.nil?
                   render json: { errors: "invalid number of repeated weeks" }, status: :unprocessable_entity
                 end
 
-                practice_time_mult = practice_params[:practice_time] + ( repeat_count * week_offset )
+                puts practice_datetime
+                practice_time_mult = DateTime.strptime(practice_datetime, "%d/%m/%Y, %H:%M:%S %z") + (7 * repeat_count)# add 7 for 7 days in a week
+                puts practice_time_mult
 
-                current_practice = Practice.new(team_id: practice_params[:team_id], duration: practice_params[:duration], difficulty: practice_params[:difficulty], practice_time: practice_time_mult)
+                current_practice = Practice.new(team_id: practice_params[:team_id], duration: practice_duration, difficulty: practice_difficulty, practice_time: practice_time_mult)
                 if current_practice.valid?
                   all_practices << current_practice
                 else
@@ -98,13 +110,13 @@ module Api::V1
                   break
                 end
               end
-
               # delay saving all of them, so we dont add some and not all
               all_practices.each{|p| p.save!}
-              render json: all_practices, status: :created, location: [:v1, all_practices]
+              all_practices = {practices: all_practices}
+              render json: all_practices, status: :created
 
             else
-              @practice = Practice.new(practice_params)
+              @practice = Practice.new(team_id: practice_params[:team_id], duration: practice_duration, difficulty: practice_difficulty, practice_time: practice_params[:practice_time])
               if @practice.save
                   render json: @practice, status: :created, location: [:v1, @practice]
               else
@@ -115,7 +127,7 @@ module Api::V1
 
         # PATCH/PUT /practices/1
         def update
-            if @practice.update(practice_params)
+            if @practice.update(team_id: practice_params[:team_id], duration: practice_duration, difficulty: practice_difficulty,  practice_time: practice_params[:practice_time])
                 render json: @practice
             else
                 render json: @practice.errors, status: :unprocessable_entity
