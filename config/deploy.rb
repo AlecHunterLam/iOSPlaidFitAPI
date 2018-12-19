823a1531f79c456db93170a30d3152cc4b92a1c6
Alec Lam <alechunterlam@gmail.com>
2018-12-18 23:36:08 -0500

    Made deployment adjustments

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,56 +1,84 @@
-require 'bundler/capistrano'
-# require 'puma/capistrano'
+# Change these
+server 'plaidfit.online', port: 8080, roles: [:web, :app, :db], primary: true
 
-server 'plaidfit.online', :web, :app, :db, primary: true
+set :repo_url,        'git@github.com:wchu248/iOSPlaidFitAPI.git'
+set :application,     'iOSPlaidFitAPI'
+set :user,            'wcalat'
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
 
-set :application, 'iOSPlaidFitApi'
-set :user, 'wcalat'
-set :group, 'admin'
-set :deploy_to, "/home/#{user}/deploy/apps/#{application}"
+# Don't change these unless you know what you're doing
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
-set :scm, 'git'
-set :git_enable_submodules, 1
-set :deploy_via, :remote_cache
-set :repository, "git@github.com:wchu248/iOSPlaidFitAPI.git"
-set :branch, 'master'
+## Defaults:
+# set :scm,           :git
+# set :branch,        :master
+# set :format,        :pretty
+# set :log_level,     :debug
+# set :keep_releases, 5
 
+## Linked Files & Directories (Default None):
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-set :use_sudo, false
-
-# whenever configuration (for cron jobs)
-# require 'whenever/capistrano'
-# set :whenever_command, 'bundle exec whenever'
-
-# share public/uploads
-set :shared_children, shared_children + %w{public/uploads}
-
-# allow password prompt
-default_run_options[:pty] = true
-
-# turn on key forwarding
-ssh_options[:forward_agent] = true
-
-# keep only the last 5 releases
-after 'deploy', 'deploy:cleanup'
-after 'deploy:restart', 'deploy:cleanup'
-
-namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
   end
-  # task :symlink_shared do
-  #   run "ln -s ~/apps/CMUPlayerReadinessAPI/shared/secrets.yml ~/apps/CMUPlayerReadinessAPI/releases/#{release_name}/config/"
-  # end
+
+  before :start, :make_dirs
 end
 
-before "deploy:assets:precompile", "deploy:symlink_shared"
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
 
-# before "deploy:restart", "deploy:symlink_shared"
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
 
-# task :symlink_config, roles: :app do
-#   run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-#   run "ln -nfs #{shared_path}/config/secrets.yml #{release_path}/config/secrets.yml"
-# end
-# after "deploy:finalize_update", "deploy:symlink_config"
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
+end
+
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
