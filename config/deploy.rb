46c0fff5a2c489ab2710df0b27b957785975b5c9
Alec Lam <alechunterlam@gmail.com>
2018-12-19 13:47:13 -0500

    added sudo to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,10 +9,11 @@ set :puma_workers,    0
 
 set :rvm_map_bins, %w{gem rake ruby rails bundle}
 set :default_shell, "/bin/bash -l"
+set :rvm_type, :user
 
 # Don't change these unless you know what you're doing
 set :pty,             true
-set :use_sudo,        false
+set :use_sudo,        true
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
