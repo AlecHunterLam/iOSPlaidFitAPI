c76a29ef9f5ea9fa786ade7790c12d54f58107e3
Alec Lam <alechunterlam@gmail.com>
2018-12-19 13:30:42 -0500

    Added mapping

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,8 @@ set :user,            'wcalat'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
+set :rvm_map_bins, %w{gem rake ruby rails bundle}
+
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
