0cdfd246eb0236d1e5d1f4a242f80ee921a66ff2
Alec Lam <alechunterlam@gmail.com>
2018-12-19 13:37:30 -0500

    added bash start

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
 set :rvm_map_bins, %w{gem rake ruby rails bundle}
+set :default_shell, "/bin/bash -l"
 
 # Don't change these unless you know what you're doing
 set :pty,             true
