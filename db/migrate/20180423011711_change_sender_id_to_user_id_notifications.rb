cceffa01f2b0f007dbb7d7c8fb69a6a02b096df4
Alec Lam <alechunterlam@gmail.com>
2019-01-03 12:41:56 -0500

    Removed migration

--- db/migrate/20180423011711_change_sender_id_to_user_id_notifications.rb
+++ db/migrate/20180423011711_change_sender_id_to_user_id_notifications.rb
@@ -1,5 +1,5 @@
 class ChangeSenderIdToUserIdNotifications < ActiveRecord::Migration[5.1]
   def change
-    rename_column :notifications, :sender_id, :user_id
+    # rename_column :notifications, :sender_id, :user_id
   end
 end
